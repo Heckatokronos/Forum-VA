8acc5dec5249ceb8f6b3e0fde882019d335ca97c
”Alexander <omgout.200@gmail.com>
2022-08-03 15:47:53 +0700

    fix recovery

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -45,15 +45,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'vaforum9@gmail.com'}
+  config.action_mailer.default_options = {from: 'omgout.200@gmail.com'}
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'vaforum9@gmail.com',
-  password:            'SLp46MVvpMxqXkp',
+  user_name: 'omgout.200@gmail.com',
+  password:            Rails.application.credentials.gmail[:password],
   authentication:       'plain',
   enable_starttls_auto: true  }
   
