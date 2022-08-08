bb54f378958c7b119b0ac69758832c47b0c883a1
”Alexander <omgout.200@gmail.com>
2022-08-08 15:09:46 +0700

    WYSIWYG, USER ACCESS

--- server/app/controllers/posts_controller.rb
+++ server/app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /posts or /posts.json
   def index
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title)
+      params.require(:post).permit(:title, :body)
     end
 end
